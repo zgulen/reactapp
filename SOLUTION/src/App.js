0d0e75f8ec6dd25fbcc466f5f5e7f963c9ed621d
zgulen <gulenzubeyir@gmail.com>
2022-06-25 07:21:30 +0300

    project folder designed

---
+++ SOLUTION/src/App.js
@@ -0,0 +1,15 @@
+import Card from "../src/components/cards/Card"
+import Header from "../src/components/header/Header"
+import Navbar from "../src/components/navbar/Navbar"
+import data from "../src/components/helper/data"
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
